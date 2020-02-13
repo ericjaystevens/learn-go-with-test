6a99fa3e14ef9339e7f07d3b3a9b42951ba21a6c
Eric Jay Stevens <ericjaystevens@gmail.com>
2020-02-13 18:53:59 +0000

    calculate are of cicle with structs

--- shapes/rectangles.go
+++ shapes/rectangles.go
@@ -1,18 +1,27 @@
 package shapes
 
 import(
+	"math"
 )
 type Rectangle struct {
 	length float64
 	width float64
 }
 
+type Circle struct {
+	radius float64
+}
+
 // PerRectangle returnts the perimeter of a rectangel based on length and height
-func PerRectangle(height int, length int ) (perimter int){
-	perimter = 2 * height + 2 * length
+func (r Rectangle )PerRectangle() (perimter float64){
+	perimter = 2 * r.width + 2 * r.length
 	return
 }
 
-func AreaRectangle(rec Rectangle) (area float64){
-	return rec.width * rec.length
+func (c Circle) Area() (area float64){
+	return c.radius * c.radius * math.Pi
+}
+
+func (r Rectangle) Area() (area float64){
+	return (r.length + r.width)
 }
--- shapes/rectangles_test.go
+++ shapes/rectangles_test.go
@@ -6,22 +6,32 @@ import(
 
 
 func TestPerRectangle(t *testing.T){
-	length := 2
-	height := 2
-	got := PerRectangle(height,length)
-	want := 8
-
-	if got != want {
-		t.Errorf("got: %d, want %d", got,want)
-	}
-}
-
-func TestAreaRectangle(t *testing.T){
-	rec := Rectangle{2.0,2.0}
-	got := AreaRectangle(rec)
-	want := 4.0
+	rec := Rectangle{2,2}
+	got := rec.PerRectangle()
+	want := 8.0
 
 	if got != want {
 		t.Errorf("got: %.2f, want %.2f", got,want)
 	}
+}
+
+func TestArea(t *testing.T){
+	t.Run("rectange", func(t *testing.T){
+		rec := Rectangle{2.0,2.0}
+		got := rec.Area()
+		want := 4.0
+
+		if got != want {
+			t.Errorf("got: %.2f, want %.2f", got,want)
+		}
+	})
+	t.Run("circle", func(t *testing.T){
+		cir := Circle{10.0}
+		got := cir.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got: %g, want %g", got,want)
+		}
+	})
 } 
