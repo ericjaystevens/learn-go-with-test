710b3b40943a84b8fb78a917403d075d478dc5f4
Eric Jay Stevens <ericjaystevens@gmail.com>
2020-02-13 17:57:38 +0000

    area of rectangles

--- shapes/rectangles.go
+++ shapes/rectangles.go
@@ -2,9 +2,17 @@ package shapes
 
 import(
 )
+type Rectangle struct {
+	length float64
+	width float64
+}
 
 // PerRectangle returnts the perimeter of a rectangel based on length and height
 func PerRectangle(height int, length int ) (perimter int){
 	perimter = 2 * height + 2 * length
 	return
+}
+
+func AreaRectangle(rec Rectangle) (area float64){
+	return rec.width * rec.length
 }
--- shapes/rectangles_test.go
+++ shapes/rectangles_test.go
@@ -4,6 +4,7 @@ import(
 	"testing"
 )
 
+
 func TestPerRectangle(t *testing.T){
 	length := 2
 	height := 2
@@ -14,3 +15,13 @@ func TestPerRectangle(t *testing.T){
 		t.Errorf("got: %d, want %d", got,want)
 	}
 }
+
+func TestAreaRectangle(t *testing.T){
+	rec := Rectangle{2.0,2.0}
+	got := AreaRectangle(rec)
+	want := 4.0
+
+	if got != want {
+		t.Errorf("got: %.2f, want %.2f", got,want)
+	}
+} 
