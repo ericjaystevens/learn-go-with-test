baf39b5bff74dcc09f58e46ac6e05a00b9e7a007
Eric Jay Stevens <ericjaystevens@gmail.com>
2020-02-14 16:11:27 +0000

    bitcoin wallet works

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,18 +1,39 @@
 package wallet
 
-import(
+import (
+	"errors"
+	"fmt"
 )
 
-type Wallet struct{
+type Wallet struct {
 	ballance Bitcoin
 }
 
 type Bitcoin int
 
-func (w *Wallet) GetBallance() Bitcoin{
+type Stringer interface {
+	String() string
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+
+	if amount < w.GetBallance() {
+		w.ballance -= amount
+		return nil
+
+	} else {
+		return errors.New("Account does not have suffient funds")
+	}
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) GetBallance() Bitcoin {
 	return w.ballance
 }
 
-func (w *Wallet) Deposit(amount Bitcoin){
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.ballance += amount
 }
--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -1,16 +1,34 @@
 package wallet
 
-import(
+import (
 	"testing"
 )
 
-func TestWallet(t *testing.T){
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
-	got := wallet.GetBallance()
-	want := Bitcoin(10)
+func TestWallet(t *testing.T) {
 
-	if got != want{
-		t.Errorf("got: %d wanted: %d", got, want)
+	assertBallance := func(t *testing.T, w Wallet, want Bitcoin) {
+		t.Helper()
+
+		if w.GetBallance() != want {
+			t.Errorf("got: %s wanted: %s", w.GetBallance(), want)
+		}
 	}
+	t.Run("deposit and ballance", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBallance(t, wallet, Bitcoin(10))
+
+	})
+	t.Run("withdraw and ballance", func(t *testing.T) {
+		wallet := Wallet{ballance: 10}
+		wallet.Withdraw(Bitcoin(5))
+		assertBallance(t, wallet, Bitcoin(5))
+	})
+	t.Run("overdraw", func(t *testing.T) {
+		wallet := Wallet{ballance: 5}
+		e := wallet.Withdraw(Bitcoin(10))
+		if e == nil {
+			t.Errorf("Overdraw Error should have been returned, current ballance: %s", wallet.GetBallance())
+		}
+	})
 }
